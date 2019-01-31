8e585a49457996c53cafb388c192bfe0e3dcda42
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-30 23:15:51 -0500

    added test file

---
+++ enhancer.test.js
@@ -0,0 +1,17 @@
+const enhancer = require('./enhancer.js');
+
+describe('the enhancer module', () => {
+
+  describe('the success function', () => {
+
+  });
+
+  describe('the fail function', () => {
+
+  });
+
+  describe('the repair function', () => {
+
+  });
+
+});
