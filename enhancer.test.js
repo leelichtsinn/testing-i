42821a9459eca7d8277fc30160aa1bb1bd22e846
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-01-30 23:28:24 -0500

    added items array for testing

--- enhancer.test.js
+++ enhancer.test.js
@@ -15,3 +15,37 @@ describe('the enhancer module', () => {
   });
 
 });
+
+
+const items = [
+  {
+    name: 'longsword',
+    type: 'weapon',
+    durability: 70,
+    enhancement: '14'
+  },
+  {
+    name: 'dagger',
+    type: 'weapon',
+    durability: 98,
+    enhancement: '3'
+  },
+  {
+    name: 'vest',
+    type: 'armor',
+    durability: 100,
+    enhancement: '0'
+  },
+  {
+    name: 'steel helmet',
+    type: 'armor',
+    durability: 85,
+    enhancement: '5'
+  },
+  {
+    name: 'excalibur',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'PEN'
+  },
+]
